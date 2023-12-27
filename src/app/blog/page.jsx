4c4e92b17ba95075a280689c3f8e4e82ef563138
Aliaksandr <djvalentinoff@gmail.com>
2023-12-27 14:59:09 +0300

    feat: add content portfolio and blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link className={styles.container} href="block/test1" key="1">
+        <div className={styles.imageContainer}>
+          <Image 
+            className={styles.image}
+            src="https://img.freepik.com/free-vector/tiny-house-concept-illustration_114360-9087.jpg?size=626&ext=jpg&ga=GA1.1.465634580.1703669057&semt=sph" 
+            width={400}
+            height={250}
+            alt="" 
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      {/* <Link className={styles.container} href="block/test2" key="2">
+        <div className={styles.imageContainer}>
+          <Image 
+            className={styles.image}
+            src="https://img.freepik.com/free-vector/tiny-house-concept-illustration_114360-9087.jpg?size=626&ext=jpg&ga=GA1.1.465634580.1703669057&semt=sph" 
+            width={400}
+            height={250}
+            alt="" 
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link> */}
+    </div>
   )
 }
 
