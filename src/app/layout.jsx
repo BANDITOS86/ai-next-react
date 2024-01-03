94fa0654beb47af08549e0f9608614a457dab6e6
Aliaksandr <djvalentinoff@gmail.com>
2024-01-03 16:29:30 +0300

    feat: add dark and light mode

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,9 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { ThemeProvider } from "../context/ThemeContext";
 import Navbar from '@components/navbar/Navbar'
-import Footer from '../components/footer/Footer'
+import Footer from '@components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,11 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container">
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
+        <ThemeProvider>
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
