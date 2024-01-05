d03ee691e9723bafdcbe14512be6ac8ade3b1293
Aliaksandr <djvalentinoff@gmail.com>
2024-01-05 17:52:19 +0300

    feat: add auth google

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,8 +2,11 @@
 
 import useSWR from 'swr'
 import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
 
 const Dashboard = () => {
+  const session = useSession()
+  console.log(session);
   // const [data, setData] = useState([]);
   // const [error, setError] = useState(false);
   // const [isLoading, setIsLoading] = useState(true)
@@ -30,7 +33,7 @@ const Dashboard = () => {
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <div>Dashboard</div>
